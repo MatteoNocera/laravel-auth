f58a2e70a45cd3d2a113680b33c60554e8309614
MatteoNocera <matteo_nocera@tiscali.it>
2023-11-09 16:43:16 +0100

    change dashboardcontroller to stamp counts

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 use App\Models\User;
 
@@ -10,6 +11,9 @@ class DashboardController extends Controller
 {
     public function index(User $user)
     {
-        return view('admin.dashboard', compact('user'));
+        $total_projects = Project::all()->count();
+        $total_users = User::all()->count();
+
+        return view('admin.dashboard', compact('user', 'total_projects', 'total_users'));
     }
 }
